342a35919dbc1a3fdefbf00a1e02f01b4940ac11
Parva Parmar <parva004cp@gmail.com>
2024-06-18 20:14:31 +0530

    htmlVsplain

--- main.go
+++ main.go
@@ -5,11 +5,24 @@ import(
 	"net/http"
 )
 
-func helloWorldPage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w,"Hello World")
+// func handleFunction(w http.ResponseWriter, r *http.Request){
+// 	switch r.URL.Path{
+// 	case "/":
+// 		fmt.Fprint(w,"Hello World")
+// 	case "/ninja":
+// 		fmt.Fprint(w,"Wallace")
+// 	default:
+// 		fmt.Fprint(w,"Big Fat Error!")
+// 	}
+// 	fmt.Printf("Handling function with %s request\n",r.Method)
+// }
+
+func htmlVsPlain(w http.ResponseWriter, r *http.Request){
+	fmt.Println("htmlVsPlain")
+	fmt.Fprint(w,"<h1>Hello World</h1>")
 }
 
 func main() {
-	http.HandleFunc("/",helloWorldPage)
+	http.HandleFunc("/hp",htmlVsPlain)
 	http.ListenAndServe("",nil)
 }
